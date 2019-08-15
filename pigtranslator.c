00ea1ac1a91cc2580ae43eae748d65ad0db900b2
Brandon Fitzpatrick <49811439+brandonfitzy@users.noreply.github.com>
2019-08-15 12:40:53 -0400

    Update pigtranslator.c

--- pigtranslator.c
+++ pigtranslator.c
@@ -1,50 +1,40 @@
-#include <stdio.h>
+/*Pig Latin Translator*/
 
-main () {
-	char alphabet[26]={"abcdefghijklmnopqrstuvwxyz"}
-	printf("Would you like to encrypt or translate?");
-	char choice[20];
-	scanf('%s',choice);
-	if (choice=="encrypt"){
-		int n;
-		printf("How many places over would you like to shift?");
-		scanf('%d', n);
-		printf("What word would you like encrypted?");
-		char word[20];
-		scanf('%s', word);
-		int i; int j;
-		char newword[20];
-		for (i=0;i<20;i++){
-			for (j=0;j<26;j++){
-				if (word[i]==alphabet[j]{
-					newword[i]=alphabet[j+n];
-				}
-				
-			}
-			
-		}
-		printf("Your encrypted word is: %s", newword);
-	
-	} else {
-		int n;
-		printf("How many places over is the shift?");
-		scanf('%d',n);
-		printf("What word would you like translated?");
-		char word[20];
-		scanf('%s', word);
-		for (i=0;i<20;i++){
-			for (j=0;j<26;j++){
-				if (word[i]==alphabet[j]{
-					newword[i]=alphabet[j-n];
-				}
-				
-			}
-			
-		}
-		printf("Your translated word is: %s", newword);
+#include<stdio.h>
+#include <string.h>
+
+main(){
+	char word[]="Brandon";
+/*	printf("What is the word you would like to translate? \n");  <----the goal is to submitt your own words (latter sentences)
+	scanf("%s", word); */
+	int i;
+	int s=0;
+	for (i=0; i<strlen(word); i++){
+	    if (word[i]=='a'||word[i]=='e'||word[i]=='i'||word[i]=='o'||word[i]=='u'||word[i]=='y'){
+	        break;
+	    } else {
+	        s++;
+	    }
 	}
-		
+	char firstchars[s];
+	int k;
+	for (k=0;k<s;k++){
+	    firstchars[k]=word[k];
+	}
+	int lastindex=strlen(word)-1;
+	char newword[strlen(word)];
+	int j;
+	for (j=0; j<=lastindex; j++) {
+		if (j!=lastindex){
+			newword[j]=word[j+s];
+		} else {
+			strcat(newword, firstchars);
+			newword[strlen(word)]='a';
+
+		}
+	}
+	printf("%s \n", newword);
 	
-	
-return 0;	
+return 0;
+
 }
